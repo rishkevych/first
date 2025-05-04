56542a8fafe0e97b6ca8454c3ac8650c1de5a1d7
Rishkevych <rishkevychdenis@gmail.com>
2025-05-04 11:50:16 +0300

    Add files via upload

---
+++ kitty.py
@@ -0,0 +1,10 @@
+class Cat:
+    def __init__(self):
+        self.height = 30
+        self.weight = 5
+        self.paws = 4
+        self.eyes = 2
+    def printer(self):
+        print(self.height, self.weight, self.paws, self.eyes)
+a = Cat()
+a.printer()
